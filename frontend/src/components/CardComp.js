cae689c6d669356d295c2fd12a3290c7060a5db3
nukeTK <tarandeepsingh1809@gmail.com>
2023-01-28 20:01:51 +0530

    frontend

---
+++ frontend/src/components/CardComp.js
@@ -0,0 +1,38 @@
+import { Card, CardContent, CardMedia, Divider, Grid, Typography } from '@mui/material'
+import React from 'react'
+
+const CardComp = ({post}) => {
+  return (
+    <Grid post sx={{marginLeft:"5px"}}>
+    <Card
+      elevation={3}
+      sx={{ width: 380, height: 500, borderRadius: "20px" }}
+    >
+      <CardMedia
+        component="img"
+        alt="AI"
+        height="300"
+        image=""
+        sx={{
+          objectFit: "cover",
+          objectPosition: "top",
+          transition: "0.4s ease",
+          borderStartStartRadius: "20px",
+          "&:hover": {
+            transform: "scale(1.10)",
+            cursor: "pointer",
+          },
+        }}
+      />
+      <Divider />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          "Any"
+        </Typography>
+      </CardContent>
+    </Card>
+  </Grid>
+  )
+}
+
+export default CardComp
